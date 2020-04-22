471d02b5a54281d21a3ea5a66bc4dfe8ccae1bd0
Alan Shindelman <ajs573@scarletmail.rutgers.edu>
2020-04-22 16:28:06 -0400

    Task 3 WIP

--- compress.ps1
+++ compress.ps1
@@ -1,6 +1,5 @@
 <# Alan Shindelman
 PowerShell DB Dump Compression
-Task 1 of x
 #>
 
 <# Task 1: 
@@ -22,7 +21,8 @@ Task 1 of x
 <# Task 3:
 - Extend the script you wrote for task 2 by using PowerShell to compress every file your script found into the .zip file format.
 - Have the .zip files placed into a new folder called "archived"
-- In addition to writing the list of files to that master-list.txt, the script will have generated .zip files for each file it found
+- In addition to writing the list of files to that master-list.txt, the script will have generated .zip files for each file it found and add them to the master list
+    - Make master list format better?
 #>
 
 function Find-Path {
@@ -38,12 +38,36 @@ function Find-Path {
             })]
         [System.IO.FileInfo]$Path
     )
-    $dbDumpPath = $Path.toString() + "\dbdump"
+    # Created Variable
+    $dbDumpPath = $Path.toString() + "\dbdump" ## dbDumpPath is = to C:\users\ajs573\Documents\dbdump
+    # $dbArchivedPath = $dbDumpPath.toString() + "\archived" ## dbArchievedPath is = to C:\users\ajs573\Documents\dbdump\archived 
+    $MasterList = "C:\Users\ajs573\Documents\DB-Dump-Compression-Repo\Outputs\dbdump_master_list.txt" ## Master list of outputted files from dbdump folder and zipped files
+    
+    # Find dbdump folder files / add to master list / zip found files / add zipped files to master list
     if ( -Not (Get-ChildItem -Path $Path -Directory | Where-Object {$_.PSIsContainer -eq $true -and $_.Name -Like "dbdump"} )) {
         throw "There is no dbdump folder in the path $Path"
     }
-    else { Get-ChildItem -Path $dbDumpPath -File | Format-Table -HideTableHeaders FullName | out-file C:\Users\ajs573\Documents\DB-Dump-Compression-Repo\Outputs\dbdump_master_list.txt
-    } 
+    else { 
+        Get-ChildItem -Path $dbDumpPath -File | Format-Table -HideTableHeaders FullName | out-file $MasterList   ## add files in dbdump to master list
+        $ArchivedFolder = New-Item -Path $dbDumpPath -Name "archived" -ItemType "directory" ## Creates new archived folder
+        Get-ChildItem -Path $dbDumpPath -File | Compress-Archive -Update -DestinationPath ($ArchivedFolder.toString() + "\archived.zip")
+        Get-ChildItem -Path $ArchivedFolder -File -Recurse | Format-Table -HideTableHeaders FullName | out-file -append $MasterList ## add new zip files to master list
+    }
 }
 # Invoke Find-Path function with specified path location
 Find-Path -path C:\users\ajs573\Documents
+
+
+<# ## Possible zipping of individual files ##
+Add-Type -assembly 'System.IO.Compression'
+Add-Type -assembly 'System.IO.Compression.FileSystem'
+New-Item -Path $dbDumpPath -Name "archived" -ItemType "directory"
+[string]$zipFN = 'C:\users\ajs573\Documents\dbdump\archived'
+$filesToZip = (Get-ChildItem -Path $dbDumpPath -File).FullName  # | Compress-Archive -Update -DestinationPath C:\users\ajs573\Documents\dbdump\archived 
+
+[System.IO.Compression.ZipArchive]$ZipFile = [System.IO.Compression.ZipFile]::Open($zipFN,([System.IO.Compression.ZipArchiveMode]::Create))
+foreach ($fileToZip in $filesToZip) {
+    [System.IO.Compression.ZipFileExtensions]::CreateEntryFromFile($ZipFile, $fileToZip, (Split-Path $fileToZip -Leaf))
+}
+    $ZipFile.Dispose()
+#>
